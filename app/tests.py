26e47ce3c613ead1088ac8342e500e1b1939194a
Joy Christine <joychristin2@gmail.com>
2022-06-04 02:41:42 +0300

    run 2 tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from . models import Image,Profile
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.photo = Image(name='image',caption='cool image',comment='cool comment') 
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo, Image))
+
+    # # save
+    def test_save_method(self):
+        self.photo.save_photo()
+        photos = Image.objects.all()
+        self.assertTrue(len(photos)>0)
+
+        
