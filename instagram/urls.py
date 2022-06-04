678eabe481775c6a691346778a183ca52c11ca95
Joy Christine <joychristin2@gmail.com>
2022-06-04 14:39:42 +0300

    add login and logout paths

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth.views import LoginView, LogoutView
+from app import views as main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
-    path('',include('app.urls'))
+    path('',main.index,name='index'),
+    path('home/',include('app.urls')),
+    path('login/',LoginView.as_view(template_name='registration/login.html'),name='login'),
+    path('logout/',LogoutView.as_view(template_name='all/index.html'),name='logout')
+
+   
 ]
