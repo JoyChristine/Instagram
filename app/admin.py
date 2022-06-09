db8a12fffaf7fdf0ddd18dede2d57e13c999915f
Joy Christine <joychristin2@gmail.com>
2022-06-09 16:29:26 +0300

    register models

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . models import Profile, Comment,Post,Follow
 # Register your models here.
+# my models
 admin.site.register(Profile)
 admin.site.register(Follow)
 admin.site.register(Comment)
